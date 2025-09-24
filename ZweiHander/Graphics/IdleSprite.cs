6e65788e7662bcb82e5e936481beba75862ef9ef
SquareJacob <jacobascherzer@gmail.com>
2025-09-24 13:39:50 -0400

    comments for IdleSprite

--- ZweiHander/Graphics/IdleSprite.cs
+++ ZweiHander/Graphics/IdleSprite.cs
@@ -9,19 +9,53 @@ using System.Threading.Tasks;
 namespace ZweiHander.Graphics;
 public class IdleSprite : ISprite
 {
-    // The texture region containing the sprite
+    /// <summary>
+    /// The texture region containing the sprite
+    /// </summary>
     private TextureRegion _region;
-    // The SpriteBatch to draw to
+
+    /// <summary>
+    /// The SpriteBatch to draw to
+    /// </summary>
     private SpriteBatch _spriteBatch;
 
     // Default parameters for draw
+
+    /// <summary>
+    /// The color mask to apply when drawing this sprite on screen
+    /// </summary>
     private Color Color = Color.White;
+
+    /// <summary>
+    /// The amount of rotation, in radians, to apply when drawing this sprite on screen
+    /// </summary>
     private float Rotation = 0.0f;
+
+    /// <summary>
+    /// The center of rotation, scaling, and position when drawing this sprite on screen
+    /// </summary>
     private Vector2 Origin = Vector2.Zero;
+
+    /// <summary>
+    /// The amount of scaling to apply to the x- and y-axes when drawing this sprite on screen
+    /// </summary>
     private Vector2 Scale = Vector2.One;
+
+    /// <summary>
+    /// Specifies if this sprite should be flipped horizontally, vertically, or both when drawing on screen
+    /// </summary>
     private SpriteEffects Effects = SpriteEffects.None;
+
+    /// <summary>
+    /// The depth of the layer to use when drawing this sprite on screen
+    /// </summary>
     private float LayerDepth = 0.0f;
 
+    /// <summary>
+    /// Creates a new idle sprite.
+    /// </summary>
+    /// <param name="region">The texture region containing the sprite.</param>
+    /// <param name="spriteBatch">The spritebatch instance used for batching draw calls.</param>
     public IdleSprite(TextureRegion region, SpriteBatch spriteBatch)
     {
         _region = region;
