56d2fc87a493dfe1ba98334cd34a93ebc12beac8
fadgjl98 <Fadgjl98@gmail.com>
2025-09-25 17:27:29 -0400

    Added Block.cs blockfactory

---
+++ ZweiHander/Environment/Block.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using ZweiHander.Graphics;
+
+namespace ZweiHander.Environment
+{
+    public enum BlockType
+    {
+        Solid,
+        Pushable,
+        Breakable,
+        Decorative
+    }
+    public class Block
+    {
+        private BlockType _blockType;
+        private Point _gridPosition;
+        private int _gridSize;
+        private bool collision = true;
+
+        public Block(BlockType blockType, Point gridPosition, int gridSize)
+        {
+            _blockType = blockType;
+            _gridPosition = gridPosition;
+            _gridSize = gridSize;
+
+        }
+
+        public Vector2 getVectorPosition()
+        {
+            return new Vector2(_gridPosition.X * _gridSize, _gridPosition.Y * _gridSize);
+        }
+
+
+
+        public Rectangle getHitbox()
+        {
+            return new Rectangle(_gridPosition.X * _gridSize, _gridPosition.Y * _gridSize, _gridSize, _gridSize);
+        }
+
+        public bool isCollidable()
+        {
+            if (_blockType == BlockType.Decorative) { collision = false; }
+            return collision;
+        }
+        
+    }
+}
---
+++ ZweiHander/Environment/BlockFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace ZweiHander.Environment
+{
+    public class BlockFactory
+    {
+        private int _tileSize;
+        private BlockSprites _blockSprites;
+
+        public BlockFactory(int tileSize, BlockSprites blockSprites)
+        {
+            _tileSize = tileSize;
+            _blockSprites = blockSprites;
+        }
+
+        public Block CreateBlock(string type, Point gridPosition)
+        {
+            bool isSolid = true;
+            string spriteKey = type;
+
+            switch (type)
+            {
+                case "StoneBlock":
+                    isSolid = true;
+                    spriteKey = "StoneBlock";
+                    break;
+                case "PushableBlock":
+                    isSolid = false; // pushable
+                    spriteKey = "PushableBlock";
+                    break;
+                case "DecorativeBlock":
+                    isSolid = false;
+                    spriteKey = "DecorativeBlock";
+                    break;
+            }
+
+            return new Block(gridPosition, spriteKey, isSolid, _tileSize);
+        }
+    }
+}
