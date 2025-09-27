9236599217338f7f4b878839e0200bb2164a8f8e
SquareJacob <jacobascherzer@gmail.com>
2025-09-27 14:06:52 -0400

    Basic Item stuff; will implement more shortly

---
+++ ZweiHander/Items/ItemStorages/CompassItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace ZweiHander.Items.ItemStorages;
+public class CompassItem : AbstractItem
+{
+    public CompassItem()
+    {
+    }
+}
