de0327f0b5062b7cd282579a81bff09f201d5b40
Victor Zinoviev <viczinoviev@gmail.com>
2025-09-27 17:46:01 -0400

    Added Treasure sprites.

---
+++ ZweiHander/Graphics/SpriteStorages/TreasureSprites.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using ZweiHander.Graphics;
+
+namespace ZweiHander.Graphics.SpriteStorages;
+public class TreasureSprites : SpriteFactory
+{
+    private const string _definitionFile = "SpriteSheets/TreasureDefinition.xml";
+    private SpriteBatch _spriteBatch;
+    public TreasureSprites(ContentManager content, SpriteBatch spriteBatch)
+    {
+        FromFile(content, _definitionFile);
+        _spriteBatch = spriteBatch;
+    }
+    public ISprite Heart() => new IdleSprite(_regions["heart"], _spriteBatch);
+    public ISprite HeartContainer() => new IdleSprite(_regions["heart-container"], _spriteBatch);
+    public ISprite Fairy() => new IdleSprite(_regions["fairy"], _spriteBatch);
+    public ISprite Rupy() => new IdleSprite(_regions["rupy"], _spriteBatch);
+}
+
+
