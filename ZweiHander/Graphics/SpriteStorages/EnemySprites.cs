4699c3fe2f1e2e275243c7ae3cfa122f89f069d5
Victor Zinoviev <viczinoviev@gmail.com>
2025-10-03 19:40:04 -0400

    Added more enemy sprites.

--- ZweiHander/Graphics/SpriteStorages/EnemySprites.cs
+++ ZweiHander/Graphics/SpriteStorages/EnemySprites.cs
@@ -23,6 +23,20 @@ public class EnemySprites : SpriteFactory
         s.Effects = SpriteEffects.FlipHorizontally;
         return s;
     }
+    public ISprite Gel() => new AnimatedSprite(_spriteBatch, _animations["gel-move-animation"]);
+    public ISprite GoriyaDown() => new IdleSprite(_regions["goriya-down"], _spriteBatch);
+    public ISprite GoriyaUp() => new IdleSprite(_regions["goriya-up"], _spriteBatch);
+    public ISprite GoriyaRight() => new IdleSprite(_regions["goriya-right"], _spriteBatch);
+    public ISprite GoriyaLeft()
+    {
+        IdleSprite s = new IdleSprite(_regions["goriya-right"], _spriteBatch);
+        s.Effects = SpriteEffects.FlipHorizontally;
+        return s;
+    }
+    public ISprite GoriyaUse() => new IdleSprite(_regions["goriya-use"], _spriteBatch);
+    public ISprite Keese() => new AnimatedSprite(_spriteBatch, _animations["keese-move-animation"]);
+
+    public ISprite Stalfos() => new IdleSprite(_regions["stalfos"], _spriteBatch);
 
 }
 
