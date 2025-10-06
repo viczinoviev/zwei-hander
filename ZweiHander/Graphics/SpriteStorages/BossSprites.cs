ac7f209968a154e55f131c0dd74441053c67c941
Victor Zinoviev <viczinoviev@gmail.com>
2025-10-06 15:55:41 -0400

    Added Dodongo boss sprites.

--- ZweiHander/Graphics/SpriteStorages/BossSprites.cs
+++ ZweiHander/Graphics/SpriteStorages/BossSprites.cs
@@ -15,7 +15,24 @@ public class BossSprites : SpriteFactory
     }
     public ISprite Aquamentus() => new AnimatedSprite(_spriteBatch, _animations["aquamentus-animation"]);
     public ISprite AquamentusProjectile() => new AnimatedSprite(_spriteBatch, _animations["aquamentus-projectile-animation"]);
+    public ISprite DodongoDown() => new IdleSprite(_regions["dodongo-down"], _spriteBatch);
+    public ISprite DodongoDownHurt() => new AnimatedSprite(_spriteBatch, _animations["dodongo-down-hurt"]);
+    public ISprite DodongoUp() => new AnimatedSprite(_spriteBatch, _animations["dodongo-move-up-animation"]);
+    public ISprite DodongoRight() => new AnimatedSprite(_spriteBatch, _animations["dodongo-move-right-animation"]);
+    public ISprite DodongoLeft()
+    {
+        ISprite s = this.DodongoRight();
+        s.Effects = SpriteEffects.FlipHorizontally;
+        return s;
+    }
+    public ISprite DodongoRightHurt() => new IdleSprite(_regions["dodongo-right-hurt"], _spriteBatch);
+    public ISprite DodongoLeftHurt()
+    {
+        ISprite s = this.DodongoRightHurt();
+        s.Effects = SpriteEffects.FlipHorizontally;
+        return s;
+    }
 
-
+    //NOTE: There is no attack sprite for Dodongo Up
 }
 
