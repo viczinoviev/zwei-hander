1ecad7ffbefc24fd6361e143df5fa98d2b75707d
Victor Zinoviev <viczinoviev@gmail.com>
2025-10-06 15:27:14 -0400

    Added Aquamentus sprites

---
+++ ZweiHander/Graphics/SpriteStorages/BossSprites.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using ZweiHander.Graphics;
+
+namespace ZweiHander.Graphics.SpriteStorages;
+
+public class BossSprites : SpriteFactory
+{
+    private const string _definitionFile = "SpriteSheets/EnemyBossesDefinition.xml";
+    private SpriteBatch _spriteBatch;
+    public BossSprites(ContentManager content, SpriteBatch spriteBatch)
+    {
+        FromFile(content, _definitionFile);
+        _spriteBatch = spriteBatch;
+    }
+    public ISprite Aquamentus() => new AnimatedSprite(_spriteBatch, _animations["aquamentus-animation"]);
+    public ISprite AquamentusProjectile() => new AnimatedSprite(_spriteBatch, _animations["aquamentus-projectile-animation"]);
+
+
+}
+
