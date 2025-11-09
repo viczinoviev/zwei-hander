0dd0f0b288f5387db62792111fb5cf3aca2a721b
Victor Zinoviev <viczinoviev@gmail.com>
2025-11-09 16:00:42 -0500

    Added more HUD components

--- ZweiHander/Graphics/SpriteStorages/HUDSprites.cs
+++ ZweiHander/Graphics/SpriteStorages/HUDSprites.cs
@@ -14,8 +14,11 @@ public class HUDSprites : SpriteFactory
         _spriteBatch = spriteBatch;
     }
 
+    public ISprite InventoryDisplay() => new IdleSprite(_regions["inventory-display"], _spriteBatch);
+    public ISprite MapDisplay() => new IdleSprite(_regions["map-display"], _spriteBatch);
+    public ISprite HeadsUpHUD() => new IdleSprite(_regions["heads-up-hud"], _spriteBatch);
     public ISprite HeartFull() => new IdleSprite(_regions["heart-full"], _spriteBatch);
     public ISprite HeartHalf() => new IdleSprite(_regions["heart-half"], _spriteBatch);
     public ISprite HeartEmpty() => new IdleSprite(_regions["heart-empty"], _spriteBatch);
-    public ISprite HeadsUpHUD() => new IdleSprite(_regions["heads-up-hud"], _spriteBatch);
+    public ISprite BlueFrame() => new IdleSprite(_regions["blue-frame"], _spriteBatch);
 }
