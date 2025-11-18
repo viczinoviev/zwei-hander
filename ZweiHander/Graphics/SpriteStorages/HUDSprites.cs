e3465b8f0787ffa680d9788a63d8693782c62307
SquareJacob <jacobascherzer@gmail.com>
2025-11-17 20:24:01 -0500

    cleaning item selection

--- ZweiHander/Graphics/SpriteStorages/HUDSprites.cs
+++ ZweiHander/Graphics/SpriteStorages/HUDSprites.cs
@@ -20,13 +20,14 @@ public class HUDSprites : SpriteFactory
     public ISprite HeartFull() => new IdleSprite(_regions["heart-full"], _spriteBatch);
     public ISprite HeartHalf() => new IdleSprite(_regions["heart-half"], _spriteBatch);
     public ISprite HeartEmpty() => new IdleSprite(_regions["heart-empty"], _spriteBatch);
-    public ISprite BlueFrame() => new IdleSprite(_regions["blue-frame"], _spriteBatch);
     public ISprite NormalSword() => new IdleSprite(_regions["normal-sword"], _spriteBatch);
     public ISprite Bow() => new IdleSprite(_regions["bow"], _spriteBatch);
     public ISprite Digit(int digit) => new IdleSprite(_regions[digit.ToString()], _spriteBatch);
     public ISprite Number(int number, int digits = -1) => new NumberSprite(number, _spriteBatch, this, digits);
     public ISprite Map() => new IdleSprite(_regions["map"], _spriteBatch);
     public ISprite OrangeCandle() => new IdleSprite(_regions["orange-candle"], _spriteBatch);
-    public ISprite BlueSelect() => new IdleSprite(_regions["blue-select"], _spriteBatch);
+    public ISprite BlueFrame() => new IdleSprite(_regions["blue-frame"], _spriteBatch);
+    public ISprite RedFrame() => new IdleSprite(_regions["red-frame"], _spriteBatch);
     public ISprite NormalBoomerang() => new IdleSprite(_regions["normal-boomerang"], _spriteBatch);
+    public ISprite Bomb() => new IdleSprite(_regions["bomb"], _spriteBatch);
 }
--- ZweiHander/HUD/InventoryHUD.cs
+++ ZweiHander/HUD/InventoryHUD.cs
@@ -17,11 +17,11 @@ namespace ZweiHander.HUD
     public class InventoryHUD : IHUDComponent
     {
         private readonly ISprite _inventoryDisplayHUD;
-        private readonly ISprite _blueFrameHUD;
+        private readonly ISprite _redFrameHUD;
+        private readonly ISprite _swordSprite;
         private readonly Vector2 _position;
         private readonly Vector2 _relativePosition;
         private readonly Player _player;
-        private readonly Vector2 _selectedPosition = new Vector2(145,112);
         private readonly Vector2 _selectedPositionB = new Vector2(264, 464);
         private readonly Vector2 _selectedPositionA = new Vector2(312, 464);
 
@@ -32,7 +32,7 @@ namespace ZweiHander.HUD
             get => _selectedIndex;
             set
             {
-                if (value < 0 || value >= _orderedItemCount) return;
+                if (value < 0 || value >= _orderedUsableCount) return;
                 _selectedIndex = value;
             }
         }
@@ -41,7 +41,7 @@ namespace ZweiHander.HUD
         {
             do
             {
-                _selectedIndex = (_selectedIndex + 1) % _orderedItemCount;
+                _selectedIndex = (_selectedIndex + 1) % _orderedUsableCount;
             }
             while (!_acquiredItems[_selectedIndex]);
         }
@@ -50,31 +50,39 @@ namespace ZweiHander.HUD
         {
             do
             {
-                _selectedIndex = (_selectedIndex - 1 + _orderedItemCount) % _orderedItemCount;
+                _selectedIndex = (_selectedIndex - 1 + _orderedUsableCount) % _orderedUsableCount;
             }
             while (!_acquiredItems[_selectedIndex]);
         }
 
-        public OrderedItem? GetSelectedItem()
+        public OrderedUsable? GetSelectedItem()
         {
-            return (OrderedItem)_selectedIndex;
+            return (OrderedUsable)_selectedIndex;
         }
+
         /// <summary>
         /// The items in the inventory, ordered in this enum
         /// </summary>
-        public enum OrderedItem
+        public enum OrderedUsable
         {
-            Map,
-            Sword, Boomerang, Bow, Fire
+            Boomerang, Bow, Bomb, Fire
+        }
+
+        /// <summary>
+        /// The permanents in the inventory, ordered in this enum
+        /// </summary>
+        private enum OrderedPermanent
+        {
+            Map = 4
         }
 
         /// <summary>
         /// Position of each item relative to inventory sprite, ordered by enum
         /// </summary>
         private readonly List<Vector2> _itemPositions = [
-            //new(271, 63),
             new(272, 106), new(320, 106), new(368, 106), new(416, 106),
-            new(272, 146), new(320, 146), new(368, 146), new(416, 146),
+            //new(272, 146), new(320, 146), new(368, 146), new(416, 146),
+            new(271, 63)
         ];
         /// <summary>
         /// Which of the items in the enum does the player have
@@ -85,50 +93,55 @@ namespace ZweiHander.HUD
         /// </summary>
         private readonly List<ISprite> _itemSprites;
         /// <summary>
+        /// Number of permanents that CAN be displayed
+        /// </summary>
+        private readonly int _orderedPermanentCount = Enum.GetNames(typeof(OrderedPermanent)).Length;
+        /// <summary>
+        /// Number of usables that CAN be displayed
+        /// </summary>
+        private readonly int _orderedUsableCount = Enum.GetNames(typeof(OrderedUsable)).Length;
+        /// <summary>
         /// Number of items that CAN be displayed
         /// </summary>
-        private readonly int _orderedItemCount = Enum.GetNames(typeof(OrderedItem)).Length;
+        private readonly int _orderedItemCount = Enum.GetNames(typeof(OrderedPermanent)).Length + Enum.GetNames(typeof(OrderedUsable)).Length;
 
         public InventoryHUD(HUDSprites hudSprites, Vector2 position, Player player)
         {
             hudSprites = hudSprites ?? throw new ArgumentNullException(nameof(hudSprites));
             _inventoryDisplayHUD = hudSprites.InventoryDisplay();
-            _blueFrameHUD = hudSprites.BlueFrame();
+            _redFrameHUD = hudSprites.RedFrame();
             _position = position; // Position is determined by HUDManager
             _relativePosition = position - new Vector2(_inventoryDisplayHUD.Width, _inventoryDisplayHUD.Height) / 2;
             _player = player;
 
             _acquiredItems = [.. Enumerable.Repeat(false, _orderedItemCount)];
             _itemSprites = [.. Enumerable.Repeat<ISprite>(null, _orderedItemCount)];
-            _itemSprites[(int)OrderedItem.Sword] = hudSprites.NormalSword();
-            _itemSprites[(int)OrderedItem.Bow] = hudSprites.Bow();
-            _itemSprites[(int)OrderedItem.Map] = hudSprites.Map();
-            _itemSprites[(int)OrderedItem.Fire] = hudSprites.OrangeCandle();
-            _itemSprites[(int)OrderedItem.Boomerang] = hudSprites.NormalBoomerang();
+            _itemSprites[(int)OrderedUsable.Bow] = hudSprites.Bow();
+            _itemSprites[(int)OrderedPermanent.Map] = hudSprites.Map();
+            _itemSprites[(int)OrderedUsable.Fire] = hudSprites.OrangeCandle();
+            _itemSprites[(int)OrderedUsable.Boomerang] = hudSprites.NormalBoomerang();
+            _itemSprites[(int)OrderedUsable.Bomb] = hudSprites.Bomb();
+            _swordSprite = hudSprites.NormalSword();
         }
 
         public void Update(GameTime gameTime)
         {
-            _acquiredItems[(int)OrderedItem.Sword] = _player.InventoryCount(typeof(Sword)) > 0;
-            _acquiredItems[(int)OrderedItem.Bow] = _player.InventoryCount(typeof(Bow)) > 0;
-            _acquiredItems[(int)OrderedItem.Map] = _player.InventoryCount(typeof(MapItem)) > 0;
-            _acquiredItems[(int)OrderedItem.Fire] = _player.InventoryCount(typeof(Fire)) > 0;
-            _acquiredItems[(int)OrderedItem.Boomerang] = _player.InventoryCount(typeof(Boomerang)) > 0;
+            _acquiredItems[(int)OrderedUsable.Bow] = _player.InventoryCount(typeof(Bow)) > 0;
+            _acquiredItems[(int)OrderedPermanent.Map] = _player.InventoryCount(typeof(MapItem)) > 0;
+            _acquiredItems[(int)OrderedUsable.Fire] = _player.InventoryCount(typeof(Fire)) > 0;
+            _acquiredItems[(int)OrderedUsable.Boomerang] = _player.InventoryCount(typeof(Boomerang)) > 0;
+            _acquiredItems[(int)OrderedUsable.Bomb] = _player.InventoryCount(typeof(Bomb)) > 0;
         }
 
         public void Draw(SpriteBatch spriteBatch, Vector2 offset)
         {
             _inventoryDisplayHUD.Draw(_position + offset);
-            _itemSprites[(int)OrderedItem.Sword].Draw(_selectedPositionA + _relativePosition + offset);
+            _swordSprite.Draw(_selectedPositionA + _relativePosition + offset);
+            _redFrameHUD.Draw(_itemPositions[_selectedIndex] + _relativePosition + offset);
+            _itemSprites[_selectedIndex].Draw(_selectedPositionB + _relativePosition + offset);
             for (int i = 0; i < _orderedItemCount; i++)
             {
                 if (_acquiredItems[i]) _itemSprites[i].Draw(_itemPositions[i] + _relativePosition + offset);
-            }
-            if (_acquiredItems[_selectedIndex])
-            {
-                _blueFrameHUD.Draw(_itemPositions[_selectedIndex] + _relativePosition + offset);
-                _itemSprites[_selectedIndex].Draw(_selectedPosition + _relativePosition + offset);
-                _itemSprites[_selectedIndex].Draw(_selectedPositionB + _relativePosition + offset);
             }
         }
     }
