5a3d6c2b935e0a4f11c326f425e80b31711efdb6
viczinoviev <106315428+viczinoviev@users.noreply.github.com>
2025-12-09 14:30:25 -0500

    Replace numbersprites with ItemWithCount objects

--- ZweiHander/HUD/HeadsUpHUD.cs
+++ ZweiHander/HUD/HeadsUpHUD.cs
@@ -14,11 +14,12 @@ namespace ZweiHander.HUD
     public class HeadsUpHUD : IHUDComponent
     {
         private readonly ISprite _headsUpDisplayHUD;
-        private readonly NumberSprite _bombs;
+        private readonly ItemWithCount _bombs;
         private readonly NumberSprite _keys;
         private readonly NumberSprite _rupies;
         private readonly Vector2 _position;
         private readonly IPlayer _player;
+        private readonly ItemWithCount _test;
 
         public HeadsUpHUD(HUDSprites hudSprites, Vector2 position, IPlayer player)
         {
@@ -26,14 +27,14 @@ namespace ZweiHander.HUD
             _headsUpDisplayHUD = hudSprites.HeadsUpHUD();
             _position = position; // Position is determined by HUDManager
             _player = player;
-            _bombs = (NumberSprite)hudSprites.Number(0, 3);
             _keys = (NumberSprite)hudSprites.Number(0, 3);
             _rupies = (NumberSprite)hudSprites.Number(0, 3);
+            _bombs = new ItemWithCount(hudSprites, hudSprites.Bomb(), position, _player, typeof(Bomb));
         }
 
         public void Update(GameTime gameTime)
         {
-            _bombs.SetNumber(_player.InventoryCount(typeof(Bomb)));
+            _bombs.Update(gameTime);
             _keys.SetNumber(_player.InventoryCount(typeof(Key)));
             _rupies.SetNumber(_player.InventoryCount(typeof(Rupy)));
         }
@@ -41,7 +42,7 @@ namespace ZweiHander.HUD
         public void Draw(Vector2 offset)
         {
             _headsUpDisplayHUD.Draw(_position + offset);
-            _bombs.Draw(_position + new Vector2(-22, 35) + offset);
+            _bombs.Draw(_position + new Vector2(-472, -22) + offset);
             _keys.Draw(_position + new Vector2(-22, 18) + offset);
             _rupies.Draw(_position + new Vector2(-40, -16) + offset);
         }
--- ZweiHander/HUD/ItemWithCount.cs
+++ ZweiHander/HUD/ItemWithCount.cs
@@ -15,22 +15,28 @@ namespace ZweiHander.HUD
     public class ItemWithCount : IHUDComponent
     {
         private readonly ISprite _itemSprite;
-        protected readonly Vector2 _position;
-        protected readonly IPlayer _player;
-        protected readonly Type _itemType;
-        protected readonly NumberSprite _numberSprite;
+        private readonly Vector2 _position;
+        private readonly IPlayer _player;
+        private readonly Type _itemType;
+        private readonly NumberSprite _numberSprite;
+        private readonly ISprite _x;
 
-        protected ItemWithCount(ISprite itemSprite, Vector2 position, IPlayer player, Type itemType)
+        public ItemWithCount(HUDSprites _hudSprites, ISprite itemSprite, Vector2 position, IPlayer player, Type itemType)
         {
             _itemSprite = itemSprite;
             _position = position;
             _player = player;
             _itemType = itemType;
+            _numberSprite = new NumberSprite(0, _hudSprites, 2);
+            _x = _hudSprites.XSymbol();
         }
 
         public void Draw(Vector2 offset)
         {
             _itemSprite.Draw(_position + offset);
+            _x.Draw(_position + new Vector2(_itemSprite.Width, 0) + offset);
+            _numberSprite.Draw(_position + new Vector2(_itemSprite.Width + _x.Width + 8, 0) + offset);
+
         }
 
         public void Update(GameTime gameTime)
